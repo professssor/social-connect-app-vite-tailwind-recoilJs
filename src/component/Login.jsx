77dc98de633925e79521af2860cf07f62159a736
professssor <62937125+professssor@users.noreply.github.com>
2023-10-01 19:49:37 +0530

    Update Login.jsx

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -56,7 +56,7 @@ function Login() {
 
           <button
             onClick={(event) =>
-              handleLogin(event, "adarshbalika", "adarshBalika")
+              handleLogin(event, "San", "San")
             }
             type="submit"
             className="block bg-black text-white px-[2rem] py-[.8rem] rounded hover:bg-white hover:text-black border-gray-600 border-[1px] m-auto mt-4 "
